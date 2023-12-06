89c206f6f7e67224a0c7c60432b251ebba77dd92
Valentin <Chris_valentin@hotmail.com>
2023-12-06 14:06:15 +0100

    navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,13 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 export const metadata = {};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        <NavBar />
         <main>{children}</main>
       </body>
     </html>
